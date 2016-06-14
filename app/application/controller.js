eed3ebfb00814c803f274711c79427bb9819bdad
Michal Bryxí <michal.bryxi@gmail.com>
2016-06-14 09:08:51 +0100

    Incorporated reset button

--- app/application/controller.js
+++ app/application/controller.js
@@ -1,18 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  queryParams: ['color', 'seed', 'isGridVisible'],
+  // queryParams: ['color', 'seed', 'isGridVisible'],
 
-  color: '#ec933e',
-  isGridVisible: true,
-  seed: 123456789,
-  width: 8,
-  height: 8,
-  gridScale: 25,
+  initialReset: Ember.on('init', function() {
+    this.send('reset');
+  }),
 
   actions: {
     print() {
       window.print();
+    },
+    reset() {
+      this.setProperties({
+        color: '#ec933e',
+        isGridVisible: true,
+        seed: 123456789,
+        width: 8,
+        height: 8,
+        gridScale: 45
+      });
     }
   }
 });
